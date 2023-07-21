a45faec0863b9147a5241b617e4e63ccfe669ed3
fostalo <dubefostalo@gmail.com>
2023-07-21 13:00:38 -0700

    deg

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinite loop
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	printf("ïnfinite loop incoming :(\n");
+
+			i = 0;
+
+			/*while (i <10)*/
+			/*{*/
+			/*      puthar(i);*/
+			/*}*/
+			
+			printf("infinite loop avoided! \\o\n");
+			
+			return (0);
+}
